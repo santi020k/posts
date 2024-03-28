8b2f6d165de194563efff8108994d13855c7315b
Santiago Molina Orozco <santi020k@gmail.com>
2024-03-28 15:04:35 -0500

    Add buttons to index page

--- src/components/molecules/group-buttons/group-buttons.tsx
+++ src/components/molecules/group-buttons/group-buttons.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Button, { type ButtonProps, ButtonSizes, ButtonVariations } from '@/atoms/button/button'
 
 import { twMerge } from 'tailwind-merge'
@@ -7,13 +8,20 @@ interface GroupButtonsProps {
   containerProps?: React.ComponentPropsWithoutRef<'div'>;
 }
 
-const GroupButtons: React.FC<GroupButtonsProps> = ({ buttons = [], containerProps }) => (
+const defaultProps = {
+  buttons: [
+    { label: 'Primary Button', variation: ButtonVariations.Primary, size: ButtonSizes.Medium, onClick: () => {} },
+    { label: 'Secondary Button', variation: ButtonVariations.Secondary, size: ButtonSizes.Medium, onClick: () => {} }
+  ]
+}
+
+const GroupButtons: React.FC<GroupButtonsProps> = ({ buttons = defaultProps.buttons, containerProps }) => (
   <div {...containerProps} className={twMerge('flex gap-4', containerProps?.className)}>
     {buttons.map((button, index) => (
       <Button
         key={index}
         onClick={button.onClick}
-        type={button.type}
+        variation={button.variation}
         size={button.size}
       >
         {button.label}
@@ -21,13 +29,6 @@ const GroupButtons: React.FC<GroupButtonsProps> = ({ buttons = [], containerProp
     ))}
   </div>
 )
-
-GroupButtons.defaultProps = {
-  buttons: [
-    { label: 'Primary Button', variation: ButtonVariations.Primary, size: ButtonSizes.Medium, onClick: () => {} },
-    { label: 'Secondary Button', variation: ButtonVariations.Secondary, size: ButtonSizes.Medium, onClick: () => {} }
-  ]
-}
 
 export default GroupButtons
 export { type GroupButtonsProps }
